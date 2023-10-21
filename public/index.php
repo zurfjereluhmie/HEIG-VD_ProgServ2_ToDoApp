1a3af48f2b9f422f20430ac35553ef8fa0574223
Jérémie Zurflüh <zurfluh.jeremie@gmail.com>
2023-10-21 16:06:49 +0200

    Added custom php.ini file

--- public/index.php
+++ public/index.php
@@ -6,7 +6,14 @@
 <body>
     <h1>Index.php</h1>
     <a href="testMail.php">Test Mail</a>
-    <a href="testDB.php">Test DB</a>
+    <a href="testInsert.php">Test Insert</a>
+    <br>
+    <?php
+    // Check if the correct php.ini file is loaded
+    echo ini_get('upload_max_filesize') . '<br>';
+    echo php_ini_loaded_file();
+    echo phpinfo();
+    ?>
 </body>
 
 </html>
