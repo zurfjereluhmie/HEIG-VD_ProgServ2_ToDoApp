15de1fe4e0a55b48a827e2d197f2ee0deac62695
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-10-31 12:57:30 +0100

    Production ready blank DB

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,12 +4,11 @@ require_once("../app/autoload.php");
 use ch\comem\todoapp\tasks\Task;
 use ch\comem\todoapp\tasks\TaskManager;
 use ch\comem\todoapp\auth\User;
-use ch\comem\todoapp\auth\UserBuilder;
 use ch\comem\todoapp\dbCRUD\DbManagerCRUD_User;
 
 $database = DbManagerCRUD_User::getInstance();
 
-$user = $database->read(1);
+$user = $database->readUsingEmail("test1@gmail.com");
 
 echo "<pre>";
 print_r($user);
