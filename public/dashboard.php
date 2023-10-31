a537f493e523693b2012ce4a8a5aee519bd56d6c
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-10-31 13:47:06 +0100

    Created scripts for dev purposes

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once("../app/autoload.php");
 
 use ch\comem\todoapp\tasks\Task;
@@ -10,15 +11,26 @@ $database = DbManagerCRUD_User::getInstance();
 
 $user = $database->readUsingEmail("test1@gmail.com");
 
+$user->resetPassword();
+
 echo "<pre>";
 print_r($user);
 echo "</pre>";
 
 
 
+echo "<br>";
+echo "<br>";
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
+
 try {
     $user = new User("test@gmail.com", "test", "test", "test");
     print_r($user);
+    echo "<br>";
 
     $task = new Task("Test", "Test", false);
 
---
+++ public/login.php
@@ -0,0 +1,58 @@
+<?php
+
+use ch\comem\todoapp\dbCRUD\DbManagerCRUD_User;
+
+if (isset($_POST["login"])) {
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $password = htmlspecialchars($_POST["password"]);
+
+    $errorCode = 0;
+    if (!$email && !$password) {
+        $errorCode = 1;
+        header("Location: " . $_SERVER["PHP_SELF"] . "?error=" . $errorCode);
+    }
+
+    require_once("../app/autoload.php");
+    $user = DbManagerCRUD_User::getInstance()->readUsingEmail($email);
+
+    if (!$user) {
+        $errorCode = 2;
+        header("Location: " . $_SERVER["PHP_SELF"] . "?error=" . $errorCode);
+    }
+
+    if (!password_verify($password, $user->getPassword())) {
+        $errorCode = 3;
+        header("Location: " . $_SERVER["PHP_SELF"] . "?error=" . $errorCode);
+    }
+
+
+    session_start();
+    $_SESSION["user"] = [
+        "id" => $user->getId(),
+        "email" => $user->getEmail(),
+        "firstName" => $user->getFirstName(),
+        "lastName" => $user->getLastName(),
+    ];
+    header("Location: dashboard.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="<?= $_SERVER["PHP_SELF"]; ?>" method="post">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="login" value="Login">
+    </form>
+</body>
+
+</html>
