4960538524a32194a743d6e29b422ee2e3a71f5e
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-10-31 10:35:36 +0100

    Update for script testing

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,18 +1,37 @@
 <?php
 require_once("../app/autoload.php");
 
-use ch\comem\todoapp\Task;
-use ch\comem\todoapp\TaskManager;
+use ch\comem\todoapp\tasks\Task;
+use ch\comem\todoapp\tasks\TaskManager;
+use ch\comem\todoapp\auth\User;
+use ch\comem\todoapp\auth\UserBuilder;
+use ch\comem\todoapp\dbCRUD\DbManagerCRUD_User;
+
+$database = DbManagerCRUD_User::getInstance();
+
+$user = $database->read(1);
+
+echo "<pre>";
+print_r($user);
+echo "</pre>";
 
 
-$task = new Task("Test", "Test", false);
 
-echo $task->getTitle() . "<br>";
-echo $task->getDescription() . "<br>";
-echo $task->isDone() . "<br>";
+try {
+    $user = new User("test@gmail.com", "test", "test", "test");
+    print_r($user);
 
-$taskManager = TaskManager::getInstance();
+    $task = new Task("Test", "Test", false);
 
-$taskManager->add($task);
-$taskManager->displayTasks();
-$taskManager->add($task);
+    echo $task->getTitle() . "<br>";
+    echo $task->getDescription() . "<br>";
+    echo $task->isDone() . "<br>";
+
+    $taskManager = TaskManager::getInstance();
+
+    $taskManager->add($task);
+    $taskManager->displayTasks();
+    $taskManager->add($task);
+} catch (Throwable $th) {
+    echo "Erreur : " . $th->getMessage();
+}
