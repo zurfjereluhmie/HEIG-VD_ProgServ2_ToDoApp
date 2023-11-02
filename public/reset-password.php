ab7e443978ac7d1fc98a048ee9e8f7fc5cadf3f4
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-11-02 21:08:06 +0100

    Usage of php script to load redundant html strucutre

--- public/reset-password.php
+++ public/reset-password.php
@@ -98,24 +98,12 @@ if (isset($_POST['submit-email'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="fr" data-lt-installed="true">
+<html lang="en" data-lt-installed="true">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ToDoApp - Reset Password</title>
-
-    <!-- CSS Lib -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <!-- CSS Custom -->
-    <link href="styles/password-reset.css" rel="stylesheet">
-
-    <!-- icon -->
-    <link rel="icon" href="assets/icons/logo.svg">
-
-
-</head>
+<?php
+require_once 'components/head.php';
+loadHead("Reset Password", ["password-reset"]);
+?>
 
 <body class="d-flex justify-content-center align-items-center">
 
@@ -151,9 +139,10 @@ if (isset($_POST['submit-email'])) {
         </div>
     </form>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <?php
+    include_once './components/script.php';
+    loadScript();
+    ?>
 </body>
 
 </html>
