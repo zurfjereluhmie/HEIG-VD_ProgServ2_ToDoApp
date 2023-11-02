7294f6823b1880786cdefacce4e687bc7960cac6
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-11-02 21:08:16 +0100

    Update of content

--- public/components/head.php
+++ public/components/head.php
@@ -10,11 +10,9 @@ function loadHead(string $title, array $stylesheet = null): void
         <title>ToDoApp - <?= $title ?></title>
 
         <!-- CSS Lib -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <!-- CSS Custom -->
-        <link href="styles/main.css" rel="stylesheet">
-        <link href="styles/navBar.css" rel="stylesheet">
         <?php foreach ($stylesheet as $css) : ?>
             <link href="styles/<?= $css ?>.css" rel="stylesheet">
         <?php endforeach; ?>
@@ -23,8 +21,6 @@ function loadHead(string $title, array $stylesheet = null): void
         <!-- icon -->
         <link rel="icon" href="assets/icons/logo.svg">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous">
-
-
     </head>
 <?php
 }
