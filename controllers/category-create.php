34557916443fb57108f65471709b624c0fd9a909
zurfjereluhmie <zurfluh.jeremie@gmail.com>
2023-11-29 11:02:44 +0100

    Implemented Create

---
+++ controllers/category-create.php
@@ -0,0 +1,29 @@
+<?php
+
+use ch\comem\todoapp\category\CategoryBuilder;
+use ch\comem\todoapp\flash\Flash;
+use ch\comem\todoapp\category\CategoryManager;
+
+$catTitle = htmlspecialchars($_POST['cat-title']);
+$catColor = htmlspecialchars($_POST['cat-color']);
+$catDescription = htmlspecialchars($_POST['cat-description']) ?? "";
+
+if (!$catTitle || !$catColor) {
+    new Flash("category-update", TEXT['must-fill-all-fields'], "danger");
+    header("Location: /category-update.php?id=$catId");
+    die();
+}
+
+$categoryManager = CategoryManager::getInstance();
+$category = (new CategoryBuilder($catTitle, $catColor))
+    ->setDescription($catDescription)
+    ->build();
+
+if ($categoryManager->addCategory($category)) {
+    $id = $category->getId();
+    new Flash("category", TEXT['category-create'], "success");
+    header("Location: /category.php?id=$id");
+} else {
+    new Flash("category-create", TEXT['error-category-create'], "danger");
+    header("Location: /category-create.php");
+}
